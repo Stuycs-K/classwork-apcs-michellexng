d48ac061567478df48713b00bf9f641fa34dd636
Michelle <michellen83@nycstudents.net>
2024-09-27 23:15:39 -0400

    Created test cases with positive/negative integers and blank arrays for concatArray() method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -16,6 +16,7 @@ public class MyArrays{
     System.out.println("Expected: [1, 2, 3], received: " + arrayToString(nums1));
     int[] nums2 = {};
     System.out.println("Expected: [], received: " + arrayToString(nums2));
+    
     int[] ary1 = {7, 80, 900, 1000};
     System.out.println("Expected: " + arrayToString(ary1) + ", received: " + arrayToString(returnCopy(ary1)) + 
     " Same elements: " + (arrayToString(ary1).equals(arrayToString(returnCopy(ary1)))) + " Different addresses: " + (!(ary1 == returnCopy(ary1))));
@@ -34,5 +35,24 @@ public class MyArrays{
     int[] ary6 = {2, 2, 2, 2, 2};
     System.out.println("Expected: " + arrayToString(ary6) + ", received: " + arrayToString(returnCopy(ary6)) + 
     " Same elements: " + (arrayToString(ary6).equals(arrayToString(returnCopy(ary6)))) + " Different addresses: " + (!(ary6 == returnCopy(ary6))));
+
+    int[] ary1a = {1, 2, 3, 4, 5};
+    int[] ary1b = {6, 7, 8, 9, 10};
+    System.out.println("Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], received: " + arrayToString(concatArray(ary1a, ary1b));
+    int[] ary2a = {6, 5, 4, 3, 2, 1};
+    int[] ary2b = {0, -1, -2, -3, -4, -5, -6};
+    System.out.println("Expected: [6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6], received: " + arrayToString(concatArray(ary2a, ary2b));
+    int[] ary3a = {100000};
+    int[] ary3b = {1};
+    System.out.println("Expected: [100000, 1], received: " + arrayToString(concatArray(ary3a, ary3b));
+    int[] ary4a = {};
+    int[] ary4b = {98, 54, 25, 14};
+    System.out.println("Expected: [98, 54, 25, 14], received: " + arrayToString(concatArray(ary4a, ary4b));
+    int[] ary5a = {863, 2345, 1, 1};
+    int[] ary5b = {};
+    System.out.println("Expected: [863, 2345, 1, 1], received: " + arrayToString(concatArray(ary5a, ary5b));
+    int[] ary6a = {};
+    int[] ary6b = {};
+    System.out.println("Expected: [], received: " + arrayToString(concatArray(ary6a, ary6b));
   }
 }
