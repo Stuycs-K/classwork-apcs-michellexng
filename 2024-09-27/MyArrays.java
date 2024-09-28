72b8df181d5132b2656767a3371d0b3767528c6e
Michelle <michellen83@nycstudents.net>
2024-09-27 23:38:22 -0400

    Wrote concatArray() method and it works for all test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -11,7 +11,7 @@ public class MyArrays{
     return result + "]";
   }
 
-  public static int[] returnCopy(int[] ary){
+  public static int[] returnCopy(int[]ary){
     int[] copy = new int[ary.length];
     for (int i = 0; i < ary.length; i++){
       copy[i] = ary[i];
@@ -19,11 +19,25 @@ public class MyArrays{
     return copy;
   }
 
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] concat = new int[ary1.length + ary2.length];
+    int index = ary1.length;
+    for (int i = 0; i < ary1.length; i++){
+      concat[i] = ary1[i];
+    }
+    for (int n = 0; n < ary2.length; n++){
+      concat[index+n] = ary2[n];
+    }
+    return concat;
+  }
+
   public static void main(String[] args){
     int[] nums1 = {1, 2, 3};
     System.out.println("Expected: [1, 2, 3], received: " + arrayToString(nums1));
-    int[] nums2 = {};
-    System.out.println("Expected: [], received: " + arrayToString(nums2));
+    int[] nums2 = {-3, -2, -1, 0};
+    System.out.println("Expected: [-3, -2, -1, 0], received: " + arrayToString(nums2));
+    int[] nums3 = {};
+    System.out.println("Expected: [], received: " + arrayToString(nums3));
     
     int[] ary1 = {7, 80, 900, 1000};
     System.out.println("Expected: " + arrayToString(ary1) + ", received: " + arrayToString(returnCopy(ary1)) + 
@@ -46,21 +60,21 @@ public class MyArrays{
 
     int[] ary1a = {1, 2, 3, 4, 5};
     int[] ary1b = {6, 7, 8, 9, 10};
-    System.out.println("Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], received: " + arrayToString(concatArray(ary1a, ary1b));
+    System.out.println("Expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], received: " + arrayToString(concatArray(ary1a, ary1b)));
     int[] ary2a = {6, 5, 4, 3, 2, 1};
     int[] ary2b = {0, -1, -2, -3, -4, -5, -6};
-    System.out.println("Expected: [6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6], received: " + arrayToString(concatArray(ary2a, ary2b));
+    System.out.println("Expected: [6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6], received: " + arrayToString(concatArray(ary2a, ary2b)));
     int[] ary3a = {100000};
     int[] ary3b = {1};
-    System.out.println("Expected: [100000, 1], received: " + arrayToString(concatArray(ary3a, ary3b));
+    System.out.println("Expected: [100000, 1], received: " + arrayToString(concatArray(ary3a, ary3b)));
     int[] ary4a = {};
     int[] ary4b = {98, 54, 25, 14};
-    System.out.println("Expected: [98, 54, 25, 14], received: " + arrayToString(concatArray(ary4a, ary4b));
+    System.out.println("Expected: [98, 54, 25, 14], received: " + arrayToString(concatArray(ary4a, ary4b)));
     int[] ary5a = {863, 2345, 1, 1};
     int[] ary5b = {};
-    System.out.println("Expected: [863, 2345, 1, 1], received: " + arrayToString(concatArray(ary5a, ary5b));
+    System.out.println("Expected: [863, 2345, 1, 1], received: " + arrayToString(concatArray(ary5a, ary5b)));
     int[] ary6a = {};
     int[] ary6b = {};
-    System.out.println("Expected: [], received: " + arrayToString(concatArray(ary6a, ary6b));
+    System.out.println("Expected: [], received: " + arrayToString(concatArray(ary6a, ary6b)));
   }
 }
