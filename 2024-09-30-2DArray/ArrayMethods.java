5c0a9d6da416e19f018a8d9905fe49e93bda9cbe
Michelle <michellen83@nycstudents.net>
2024-09-30 19:21:37 -0400

    Fixed and added onto test cases done in class with my partner for all three methods

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -34,7 +34,7 @@ public static String arrToString(int[][]ary){
 /*Return the sum of all of the values in the 2D array */
 public static int arr2DSum(int[][]nums){
   //use a nested loop to solve this
-  return null;//place holder return value so it compiles.
+  return 1;//place holder return value so it compiles.
 }
 
 /**Rotate an array by returning a new array with the rows and columns swapped.
@@ -46,25 +46,55 @@ public static int[][] swapRC(int[][]nums){
 }
 
 public static void main(String[] args) {
-  int [][] arr = new int[[1, 4],[2, 5],[3, 6]];
-  System.out.println("Expected: [[1,4], [2,5], [3,6]]" + ", Received: " + arrToString(arr));
-  int [][] arr = new int[[-3, 5, 0],[1]];
-  System.out.println("Expected: [[-3, 5, 0], [1]]" + ", Received: " + arrToString(arr));
-  int [][] arr = new int[[7, 8, -9],[100000, 1, 3, 4, 2],[]];
-  System.out.println("Expected: [[7, 8, -9], [100000, 1, 3, 4, 2}, []]" + ", Received: " + arrToString(arr));
+  int [][] arr1a = {{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: [[1,4], [2,5], [3,6]], Received: " + arrToString(arr1a)
+  + "| Equals? " + (arrToString(arr1a).equals("[[1,4], [2,5], [3,6]]")));
+  int [][] arr1b = {{-3, 5, 0}, {1}};
+  System.out.println("Expected: [[-3, 5, 0], [1]], Received: " + arrToString(arr1b)
+  + "| Equals? " + (arrToString(arr1b).equals("[[-3, 5, 0], [1]]")));
+  int [][] arr1c = {{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
+  System.out.println("Expected: [[7, 8, -9], [100000, 1, 3, 4, 2}, []], Received: " + arrToString(arr1c)
+  + "| Equals? " + (arrToString(arr1c).equals("[[7, 8, -9], [100000, 1, 3, 4, 2}, []]")));
+  int [][] arr1d = {{}, {5, 3, 2}, {4}};
+  System.out.println("Expected: [[], [5, 3, 2], [4]], Received: " + arrToString(arr1d)
+  + "| Equals? " + (arrToString(arr1d).equals("[[], [5, 3, 2], [4]]")));
+  int [][] arr1e = {{94, 3323, 888}, {3, 2, 2}};
+  System.out.println("Expected: [[94, 3323, 888], [3, 2, 2]], Received: " + arrToString(arr1e)
+  + "| Equals? " + (arrToString(arr1e).equals("[[94, 3323, 888], [3, 2, 2]]")));
+  int [][] arr1f = {{-13464, 2}, {1093, 23234, 3}};
+  System.out.println("Expected: [[-13464, 2], [1093, 23234, 3]], Received: " + arrToString(arr1f)
+  + "| Equals? " + (arrToString(arr1f).equals("[[-13464, 2], [1093, 23234, 3]]")));
 
-  int [][] arr = new int[[1, 4],[2, 5],[3, 6]];
-  System.out.println("Expected: 21" + ", Received: " + arr2DSum(arr));
-  int [][] arr = new int[[-3, 5, 0],[1]];
-  System.out.println("Expected: 3" + ", Received: " + arr2DSum(arr));
-  int [][] arr = new int[[7, 8, -9],[100000, 1, 3, 4, 2],[]];
-  System.out.println("Expected: 100016" + ", Received: " + arr2DSum(arr));
+  int [][] arr2a = {{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: 21, Received: " + arr2DSum(arr2a) + "| Equals? " + (arr2DSum(arr2a) ==  21));
+  int [][] arr2b = {{-3, 5, 0}, {1}};
+  System.out.println("Expected: 3, Received: " + arr2DSum(arr2b) + "| Equals? " + (arr2DSum(arr2b) ==  3));
+  int [][] arr2c = {{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
+  System.out.println("Expected: 100016, Received: " + arr2DSum(arr2c) + "| Equals? " + (arr2DSum(arr2c) ==  100016));
+  int [][] arr2d = {{}, {}, {}};
+  System.out.println("Expected: 0, Received: " + arr2DSum(arr2d) + "| Equals? " + (arr2DSum(arr2d) ==  0));
+  int [][] arr2e = {{0}, {0, 0, 2}, {0, 0, 0, 0}};
+  System.out.println("Expected: 2, Received: " + arr2DSum(arr2e) + "| Equals? " + (arr2DSum(arr2e) ==  2));
+  int [][] arr2f = {{-3}, {-5}, {8, 0}};
+  System.out.println("Expected: 0, Received: " + arr2DSum(arr2f) + "| Equals? " + (arr2DSum(arr2f) ==  2));
 
-  int [][] arr = new int[[1, 4],[2, 5],[3, 6]];
-  System.out.println("Expected: [[1, 2, 3], [4, 5, 6]]" + ", Received: " + swapRC(arr));
-  int [][] arr = new int[[-3, 5],[1, 2]];
-  System.out.println("Expected: [[-3, 1], [5, 2]]" + ", Received: " + swapRC(arr));
-  int [][] arr = new int[[7, 8, -9],[100000, 1, 3]];
-  System.out.println("Expected: [[7, 100000], [8, 1], [-9, 3]]" + ", Received: " + swapRC(arr));
+  int [][] arr3a = {{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: [[1, 2, 3], [4, 5, 6]], Received: " + arrToString(swapRC(arr3a)) 
+  + "| Equals? " + (arrToString(swapRC(arr3a)).equals("[[1, 2, 3], [4, 5, 6]]")));
+  int [][] arr3b = {{-3, 5}, {1, 2}};
+  System.out.println("Expected: [[-3, 1], [5, 2]], Received: " + arrToString(swapRC(arr3b))
+  + "| Equals? " + (arrToString(swapRC(arr3b)).equals("[[1, 2, 3], [4, 5, 6]]")));
+  int [][] arr3c = {{7, 8, -9}, {100000, 1, 3}};
+  System.out.println("Expected: [[7, 100000], [8, 1], [-9, 3]], Received: " + arrToString(swapRC(arr3c))
+  + "| Equals? " + (arrToString(swapRC(arr3c)).equals("[[7, 100000], [8, 1], [-9, 3]]")));
+  int [][] arr3d = {{3, 2, 6, 8}, {4, 5, 10, 14}};
+  System.out.println("Expected: [[3, 4], [2, 5], [6, 10], [8, 14]], Received: " + arrToString(swapRC(arr3d))
+  + "| Equals? " + (arrToString(swapRC(arr3d)).equals("[[3, 4], [2, 5], [6, 10], [8, 14]]")));
+  int [][] arr3e = {{7}, {11}, {24}};
+  System.out.println("Expected: [[7, 11, 24]], Received: " + arrToString(swapRC(arr3e))
+  + "| Equals? " + (arrToString(swapRC(arr3e)).equals("[[7, 11, 24]]")));
+  int [][] arr3f = {{8, 8, 8}, {8, 8, 8}, {8, 8, 8}};
+  System.out.println("Expected: [[8, 8, 8], [8, 8, 8], [8, 8, 8]], Received: " + arrToString(swapRC(arr3f))
+  + "| Equals? " + (arrToString(swapRC(arr3f)).equals("[8, 8, 8], [8, 8, 8], [8, 8, 8]]")));
 }
 }
