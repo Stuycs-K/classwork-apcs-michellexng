60dcf309bfb07b58b2d74306a9ce66d57ba216b8
Michelle <michellen83@nycstudents.net>
2024-10-01 19:33:41 -0400

    Copied over funcntional replaceNegative() from paired programmming in class

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -64,56 +64,93 @@ public static int[][] swapRC(int[][]nums){
   return swapped;
 }
 
+//3. Modify a given 2D array of integer as follows:
+//Replace all the negative values:
+//-When the row number is the same as the column number replace
+//that negative with the value 1
+//-All other negatives replace with 0
+public static void replaceNegative(int[][] vals){ 
+  for (int i = 0; i < vals.length; i++) {
+    for (int i2 = 0; i2 < vals[i].length; i2++) {
+      if (vals[i][i2] < 0) {
+        if (i == i2) {
+          vals[i][i2] = 1;
+        }
+        else {
+          vals[i][i2] = 0;
+        }
+      }
+    }
+  }
+}
+
+//4. Make a copy of the given 2d array.
+//When testing : make sure that changing the original does NOT change the copy.
+//DO NOT use any built in methods that "copy" an array.
+//You SHOULD write a helper method for this.
+//If you don't see a good way to do that, you should stop and look at prior methods.
+public static int[][] copy(int[][] nums){
+  return null;//placeholder so it compiles
+}
+
 public static void main(String[] args) {
-  int [][] arr1a = {{1, 4}, {2, 5}, {3, 6}};
-  System.out.println("Expected: [[1, 4], [2, 5], [3, 6]], Received: " + arrToString(arr1a)
-  + "| Equals? " + (arrToString(arr1a).equals("[[1, 4], [2, 5], [3, 6]]")));
-  int [][] arr1b = {{-3, 5, 0}, {1}};
-  System.out.println("Expected: [[-3, 5, 0], [1]], Received: " + arrToString(arr1b)
-  + "| Equals? " + (arrToString(arr1b).equals("[[-3, 5, 0], [1]]")));
-  int [][] arr1c = {{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
-  System.out.println("Expected: [[7, 8, -9], [100000, 1, 3, 4, 2], []], Received: " + arrToString(arr1c)
-  + "| Equals? " + (arrToString(arr1c).equals("[[7, 8, -9], [100000, 1, 3, 4, 2], []]")));
-  int [][] arr1d = {{}, {5, 3, 2}, {4}};
-  System.out.println("Expected: [[], [5, 3, 2], [4]], Received: " + arrToString(arr1d)
-  + "| Equals? " + (arrToString(arr1d).equals("[[], [5, 3, 2], [4]]")));
-  int [][] arr1e = {{94, 3323, 888}, {3, 2, 2}};
-  System.out.println("Expected: [[94, 3323, 888], [3, 2, 2]], Received: " + arrToString(arr1e)
-  + "| Equals? " + (arrToString(arr1e).equals("[[94, 3323, 888], [3, 2, 2]]")));
-  int [][] arr1f = {{-13464, 2}, {1093, 23234, 3}};
-  System.out.println("Expected: [[-13464, 2], [1093, 23234, 3]], Received: " + arrToString(arr1f)
-  + "| Equals? " + (arrToString(arr1f).equals("[[-13464, 2], [1093, 23234, 3]]")));
+  //arrToString test cases
+  int[][] arr = new int[][]{{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: [[1, 4], [2, 5], [3, 6]], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[1, 4], [2, 5], [3, 6]]")));
+  arr = new int[][]{{-3, 5, 0}, {1}};
+  System.out.println("Expected: [[-3, 5, 0], [1]], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[-3, 5, 0], [1]]")));
+  arr = new int[][]{{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
+  System.out.println("Expected: [[7, 8, -9], [100000, 1, 3, 4, 2], []], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[7, 8, -9], [100000, 1, 3, 4, 2], []]")));
+  arr = new int[][]{{}, {5, 3, 2}, {4}};
+  System.out.println("Expected: [[], [5, 3, 2], [4]], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[], [5, 3, 2], [4]]")));
+  arr = new int[][]{{94, 3323, 888}, {3, 2, 2}};
+  System.out.println("Expected: [[94, 3323, 888], [3, 2, 2]], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[94, 3323, 888], [3, 2, 2]]")));
+  arr = new int[][]{{-13464, 2}, {1093, 23234, 3}};
+  System.out.println("Expected: [[-13464, 2], [1093, 23234, 3]], Received: " + arrToString(arr)
+  + "| Equals? " + (arrToString(arr).equals("[[-13464, 2], [1093, 23234, 3]]")));
 
-  int [][] arr2a = {{1, 4}, {2, 5}, {3, 6}};
-  System.out.println("Expected: 21, Received: " + arr2DSum(arr2a) + " | Equals? " + (arr2DSum(arr2a) ==  21));
-  int [][] arr2b = {{-3, 5, 0}, {1}};
-  System.out.println("Expected: 3, Received: " + arr2DSum(arr2b) + " | Equals? " + (arr2DSum(arr2b) ==  3));
-  int [][] arr2c = {{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
-  System.out.println("Expected: 100016, Received: " + arr2DSum(arr2c) + " | Equals? " + (arr2DSum(arr2c) ==  100016));
-  int [][] arr2d = {{}, {}, {}};
-  System.out.println("Expected: 0, Received: " + arr2DSum(arr2d) + " | Equals? " + (arr2DSum(arr2d) ==  0));
-  int [][] arr2e = {{0}, {0, 0, 2}, {0, 0, 0, 0}};
-  System.out.println("Expected: 2, Received: " + arr2DSum(arr2e) + " | Equals? " + (arr2DSum(arr2e) ==  2));
-  int [][] arr2f = {{-3}, {-5}, {8, 0}};
-  System.out.println("Expected: 0, Received: " + arr2DSum(arr2f) + " | Equals? " + (arr2DSum(arr2f) ==  0));
+  //arr2DSum test cases
+  arr = new int[][]{{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: 21, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  21));
+  arr = new int[][]{{-3, 5, 0}, {1}};
+  System.out.println("Expected: 3, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  3));
+  arr = new int[][]{{7, 8, -9}, {100000, 1, 3, 4, 2}, {}};
+  System.out.println("Expected: 100016, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  100016));
+  arr = new int[][]{{}, {}, {}};
+  System.out.println("Expected: 0, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  0));
+  arr = new int[][]{{0}, {0, 0, 2}, {0, 0, 0, 0}};
+  System.out.println("Expected: 2, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  2));
+  arr = new int[][]{{-3}, {-5}, {8, 0}};
+  System.out.println("Expected: 0, Received: " + arr2DSum(arr) + " | Equals? " + (arr2DSum(arr) ==  0));
 
-  int [][] arr3a = {{1, 4}, {2, 5}, {3, 6}};
-  System.out.println("Expected: [[1, 2, 3], [4, 5, 6]], Received: " + arrToString(swapRC(arr3a)) 
-  + " | Equals? " + (arrToString(swapRC(arr3a)).equals("[[1, 2, 3], [4, 5, 6]]")));
-  int [][] arr3b = {{-3, 5}, {1, 2}};
-  System.out.println("Expected: [[-3, 1], [5, 2]], Received: " + arrToString(swapRC(arr3b))
-  + " | Equals? " + (arrToString(swapRC(arr3b)).equals("[[-3, 1], [5, 2]]")));
-  int [][] arr3c = {{7, 8, -9}, {100000, 1, 3}};
-  System.out.println("Expected: [[7, 100000], [8, 1], [-9, 3]], Received: " + arrToString(swapRC(arr3c))
-  + " | Equals? " + (arrToString(swapRC(arr3c)).equals("[[7, 100000], [8, 1], [-9, 3]]")));
-  int [][] arr3d = {{3, 2, 6, 8}, {4, 5, 10, 14}};
-  System.out.println("Expected: [[3, 4], [2, 5], [6, 10], [8, 14]], Received: " + arrToString(swapRC(arr3d))
-  + " | Equals? " + (arrToString(swapRC(arr3d)).equals("[[3, 4], [2, 5], [6, 10], [8, 14]]")));
-  int [][] arr3e = {{7}, {11}, {24}};
-  System.out.println("Expected: [[7, 11, 24]], Received: " + arrToString(swapRC(arr3e))
-  + " | Equals? " + (arrToString(swapRC(arr3e)).equals("[[7, 11, 24]]")));
-  int [][] arr3f = {{8, 8, 8}, {8, 8, 8}, {8, 8, 8}};
-  System.out.println("Expected: [[8, 8, 8], [8, 8, 8], [8, 8, 8]], Received: " + arrToString(swapRC(arr3f))
-  + " | Equals? " + (arrToString(swapRC(arr3f)).equals("[[8, 8, 8], [8, 8, 8], [8, 8, 8]]")));
+  //swapRC test cases
+  arr = new int[][]{{1, 4}, {2, 5}, {3, 6}};
+  System.out.println("Expected: [[1, 2, 3], [4, 5, 6]], Received: " + arrToString(swapRC(arr)) 
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[1, 2, 3], [4, 5, 6]]")));
+  arr = new int[][]{{-3, 5}, {1, 2}};
+  System.out.println("Expected: [[-3, 1], [5, 2]], Received: " + arrToString(swapRC(arr))
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[-3, 1], [5, 2]]")));
+  arr = new int[][]{{7, 8, -9}, {100000, 1, 3}};
+  System.out.println("Expected: [[7, 100000], [8, 1], [-9, 3]], Received: " + arrToString(swapRC(arr))
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[7, 100000], [8, 1], [-9, 3]]")));
+  arr = new int[][]{{3, 2, 6, 8}, {4, 5, 10, 14}};
+  System.out.println("Expected: [[3, 4], [2, 5], [6, 10], [8, 14]], Received: " + arrToString(swapRC(arr))
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[3, 4], [2, 5], [6, 10], [8, 14]]")));
+  arr = new int[][]{{7}, {11}, {24}};
+  System.out.println("Expected: [[7, 11, 24]], Received: " + arrToString(swapRC(arr))
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[7, 11, 24]]")));
+  arr = new int[][]{{8, 8, 8}, {8, 8, 8}, {8, 8, 8}};
+  System.out.println("Expected: [[8, 8, 8], [8, 8, 8], [8, 8, 8]], Received: " + arrToString(swapRC(arr))
+  + " | Equals? " + (arrToString(swapRC(arr)).equals("[[8, 8, 8], [8, 8, 8], [8, 8, 8]]")));
+
+  //replaceNegative test cases
+  arr = new int[][]{{1, 4}, {-2, -5}, {3, -6}};
+  replaceNegative(arr);
+  System.out.println("Expected: [[1, 4], [0, 1], [3, 0]], Received: " + arrToString(arr));
 }
 }
