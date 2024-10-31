1d867a9b58f946148540c44d0daf80d04cf3b8a9
Michelle Ng <michellen83@nycstudents.net>
2024-10-31 11:51:05 -0400

    calculated sum of sc2

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -6,5 +6,11 @@ public class Main{
     while(sc1.hasNext()){
       System.out.println(sc1.next());
     }
+    Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+    Double sum = 0.0;
+    while(sc2.hasNextDouble()){
+      sum+=sc2.nextDouble();
+    }
+    System.out.println(sum);
   }
 }
