83af0b85deee3e629937b937be5bc8f47ae10cff
Michelle <michellen83@nycstudents.net>
2024-11-07 23:05:28 -0500

    found and explained error in assigning certain types to variables

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,6 +1,9 @@
 //Before overriding getName(), speak() returns only the name of the bird as it would if the object type was an Animal. After 
 //overriding getName(), speak() returns "The Mighty " before the name of the bird. When a method is overidden, the version 
 //of the method defined in the superclass isn't used, but instead the version of the method defined in the subclass is used.
+//Bird b2 = new Animal(...); didn't work because it isn't true that Animal is-A Bird, so Animal cannot 
+//be stored in a Bird variable type. The Bird is the subclass of the superclass Animal, so Animal may 
+//lack properties (height, color) that the Bird class has, making Animal an invalid object type here.
 public class Driver {
 
   public static void main (String[] args){
@@ -9,6 +12,10 @@ public class Driver {
     Animal b = new Bird("chirp", 10, "bob", 30.5, "blue");
     b.speak();
 
+    Animal a1 = new Animal("Hey", 15, "alex");
+    Bird b1 = new Bird("Cheep", 3, "polly", 50, "red");
+    //Bird b2 = new Animal("squawk", 16, "molly");
+    Animal a2 = new Bird("squeak", 4, "bloomie", 2, "pink");
   }
 
 
