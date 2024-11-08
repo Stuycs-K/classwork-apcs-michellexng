5301e5c941fd8e9efe7f2e94ba845c0daeefa832
Michelle <michellen83@nycstudents.net>
2024-11-07 22:57:49 -0500

    overrode getName() method, answered questions

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+//Before overriding getName(), speak() returns only the name of the bird as it would if the object type was an Animal. After 
+//overriding getName(), speak() returns "The Mighty " before the name of the bird. When a method is overidden, the version 
+//of the method defined in the superclass isn't used, but instead the version of the method defined in the subclass is used.
 public class Driver {
 
   public static void main (String[] args){
