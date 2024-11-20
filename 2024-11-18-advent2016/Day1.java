46d6e6f88f9860309b888e81c1166e2d0fc77bbb
Michelle <michellen83@nycstudents.net>
2024-11-20 00:06:50 -0500

    move method for changing direction and steps

---
+++ 2024-11-18-advent2016/Day1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Day1{
+  public static int move (String[] input){
+    int x = 0;
+    int y = 0;
+    int direction = 0;
+    int[] xDir = {0, 1, 0, -1};
+    int[] yDir = {1, 0, -1, 0};
+
+    for (int i = 0; i < input.length; i++){
+      int steps = Integer.parseInt(input[i].substring(1));
+      char turn = input[i].charAt(0);
+
+      if (turn == 'L'){
+        direction = (direction + 3) % 4;
+      }
+      else if (turn == 'R'){
+        direction = (direction + 1) % 4;
+      }
+
+      x += xDir[direction]*steps;
+      y += yDir[direction]*steps;
+      }
+    return Math.abs(x)+Math.abs(y);
+  }
+
+  public static void main(String[] args){
+    //System.out.println(move("2024d1.txt"));
+  }
+}
