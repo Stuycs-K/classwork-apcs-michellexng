3dde26cdc71cba9f4444ba87b3ab6d8bc3bcfc9c
Michelle <michellen83@nycstudents.net>
2024-11-21 22:38:16 -0500

    solved day6 pt1 w/ int array

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -3,14 +3,26 @@ import java.io.*;
 
 public class Day6{
   public static String message (String[] input){
-    int count = 0;
-    for (int i = 0; i < input[i].length; i++){
-      for (int j = 0; j < input.length; j++){
-        if (input[i].charAt(j) == input[j].charAt(j)){
+    String code = "";
+    for (int i = 0; i < input[0].length(); i++){
+      int[] count = new int[26];
 
+      for (int j = 0; j < input.length; j++){
+        char current = input[j].charAt(i);
+        count[current - 'a']++;
+        }
+
+      int maxCount = 0;
+      char letter = 'a';
+      for (int n = 0; n < 26; n++){
+        if (count[n] > maxCount){
+          maxCount = count[n];
+          letter = (char)(n + 'a');
         }
       }
+      code+=letter;
     }
+    return code;
   }
 
   public static void main(String[] args){
