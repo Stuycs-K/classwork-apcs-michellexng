b99b3793356299db540d0fc62938b8590b8d6da6
Michelle Ng <michellen83@nycstudents.net>
2024-12-06 11:47:30 -0500

    started specialAttack

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -21,23 +21,25 @@ public class Warrior extends Adventurer{
   //hurt or hinder the target adventurer
   public abstract String attack(Adventurer other){
     int lost = other.getHP()/2;
-    return other.getName() + " has lost " + lost;
+    return other.getName() + " has lost " + lost +".";
   }
 
   //heall or buff the target adventurer
   public abstract String support(Adventurer other){
     int gain = other.getmaxHP()-other.getHP();
-    return other.getName() + "has gained " + gain;
+    return other.getName() + " has gained " + gain +".";
   }
 
   //heall or buff self
   public abstract String support(){
     int gain = this.getmaxHP()-this.getHP();
-    return this.getName() + "has gained " + gain;
+    return this.getName() + " has gained " + gain+".";
   }
 
   //hurt or hinder the target adventurer, consume some special resource
   public abstract String specialAttack(Adventurer other){
-
+    int consumed = other.getSpecial()/3;
+    String specialString = this.getName() +" consumed "+consumed+" of "other.getName()+"'s "+other.getSpecialName();
+    return attack(other) + specialString;
   }
 }
