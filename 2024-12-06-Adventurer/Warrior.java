5a9008e85da1b73ba46a6cca7c5f7a41b6a410d0
Michelle Ng <michellen83@nycstudents.net>
2024-12-06 11:35:44 -0500

    wrote special skills

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,3 +1,20 @@
 public class Warrior extends Adventurer{
-  
+  private int rage;
+
+  public Warrior(String name){
+    super(name);
+  }
+
+  public String getSpecialName(){
+    return "rage";
+  }
+  public int getSpecial(){
+    return 50;
+  }
+  public void setSpecial(int n){
+    this.rage = n;
+  }
+  public int getSpecialMax(){
+    return 100;
+  }
 }
