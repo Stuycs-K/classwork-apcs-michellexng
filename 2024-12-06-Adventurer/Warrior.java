65ffb42b1eb6841732e78ea1a049c7d18c02f5cd
Michelle <michellen83@nycstudents.net>
2024-12-08 19:08:11 -0500

    added sets to the attack/heal

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -5,6 +5,10 @@ public class Warrior extends Adventurer{
     super(name);
   }
 
+  public Warrior(String name, int hp){
+    super(name, hp);
+  }
+
   public String getSpecialName(){
     return "rage";
   }
@@ -21,24 +25,29 @@ public class Warrior extends Adventurer{
   //hurt or hinder the target adventurer
   public abstract String attack(Adventurer other){
     int lost = other.getHP()/2;
+    other.setHP(other.getHP() - lost);
     return other.getName() + " has lost " + lost +".";
   }
 
   //heall or buff the target adventurer
   public abstract String support(Adventurer other){
     int gain = other.getmaxHP()-other.getHP();
+    other.setHP(other.getHP() + gain);
     return other.getName() + " has gained " + gain +".";
   }
 
   //heall or buff self
   public abstract String support(){
-    int gain = this.getmaxHP()-this.getHP();
+    int gain = this.getmaxHP()-(this.getHP()/2);
+    this.setHP(this.getHP() + gain);
     return this.getName() + " has gained " + gain+".";
   }
 
   //hurt or hinder the target adventurer, consume some special resource
   public abstract String specialAttack(Adventurer other){
     int consumed = other.getSpecial()/3;
+    this.setSpecial = this.getSpecial() + consumed;
+    other.setSpecial = other.getSpecial() - consumed;
     String specialString = this.getName() +" consumed "+consumed+" of "other.getName()+"'s "+other.getSpecialName();
     return attack(other) + specialString;
   }
