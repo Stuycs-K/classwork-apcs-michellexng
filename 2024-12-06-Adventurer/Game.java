4a295d1032c682f866fde1c9b5702c9b852503df
Michelle <michellen83@nycstudents.net>
2024-12-09 12:17:13 -0500

    helper method for dif actions

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -7,9 +7,25 @@ public class Game{
     System.out.println(p1.getName()+", "+p1.getHP()+"/"+p1.getmaxHP()+" HP, "+p1.getSpecial()+"/"+p1.getSpecialMax()+" "+p1.getSpecialName());
     System.out.println(p2.getName()+", "+p2.getHP()+"/"+p2.getmaxHP()+" HP, "+p2.getSpecial()+"/"+p2.getSpecialMax()+" "+p2.getSpecialName());
     System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
-    String action = userInput.nextLine();
-    if (action.equals("a")){
+    String move = userInput.nextLine();
+    action(p1, p2, move);
+  }
+  public static void action(Adventurer p1, Adventurer p2, String move){
+    if (move.equals("a")){
       System.out.println(p1.attack(p2));
     }
+    if (move.equals("sp")){
+      System.out.println(p1.specialAttack(p2));
+    }
+    if (move.equals("su")){
+      System.out.println(p1.support(p2));
+    }
+    if (move.equals("quit")){
+      System.out.println("Game over");
+      return;
+    }
+    // else {
+    //   System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    // }
   }
 }
